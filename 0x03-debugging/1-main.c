cc4db1c3b5a1cffec6eb36e1442f1264b54d8a35
Philisiwe Majola <majolaphilie@gmail.com>
2023-04-28 03:02:53 -0700

    one

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,12 +13,14 @@ int main(void)
 
         i = 0;
 
-        /*while (i < 10)*/
-        /*{*/
-        /*       putchar(i);*/
-        /*}*/
+        // Commenting out the infinite loop
+        // while (i < 10)
+        // {
+        //         putchar(i);
+        // }
 
         printf("Infinite loop avoided! \\o/\n");
 
         return (0);
 }
+
